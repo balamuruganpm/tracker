3ede36469dfe374a5e3cc0c0bdfa7fef6770f637
companyuseonlyrmm <companyuseonlyrmm@gmail.com>
2026-07-19 22:09:59 +0530

    v1.0.1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        ignored: ['**/node_modules/**', '**/.git/**', '**/.next/**'],
+        poll: 1000, // Check for changes every second instead of constant listening
+        aggregateTimeout: 300, // Delay rebuilds slightly to aggregate multiple changes
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
+
